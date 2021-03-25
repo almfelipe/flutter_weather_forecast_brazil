05e6bc20bc184e9cc7d0da01766df551d1fb7298
Felipe Almeida <alm.felipe@gmail.com>
2021-03-25 13:03:07 -0300

    remove import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:flutter_weather_forecast_brazil/StateBr.dart';
 import 'package:http/http.dart' as http;
 import 'package:intl/intl.dart';
 import 'package:flutter/material.dart';

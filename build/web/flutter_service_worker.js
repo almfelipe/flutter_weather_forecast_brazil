eefa771dfa3ca4c42bea9e67628580e4847224f8
Felipe Almeida <alm.felipe@gmail.com>
2021-03-26 18:18:21 -0300

    web depoly vercel v4

--- build/web/flutter_service_worker.js
+++ build/web/flutter_service_worker.js
@@ -11,9 +11,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "62c62f3d16e04a7de24422bfd6eeb0ef",
-"/": "62c62f3d16e04a7de24422bfd6eeb0ef",
-"main.dart.js": "aebfb5e16aa3a6ace89cead6e306defc",
+"index.html": "956e8d9e84c83dbb3c3f99bc11b0f108",
+"/": "956e8d9e84c83dbb3c3f99bc11b0f108",
+"main.dart.js": "ba2d396ae049a0f80cf7a7d87eed5976",
 "manifest.json": "11110ca5bf4cabf9ac2e0a8f51429a5d",
 "version.json": "898763f0690b921e2b6dc9bb91d9f800"
 };

cd707fd14dcd10e030e5f0b954bce6b993cd7dea
Felipe Almeida <alm.felipe@gmail.com>
2021-03-31 17:30:09 -0300

    remove EasyLoading

--- lib/weather_forecast_brazil_app.dart
+++ lib/weather_forecast_brazil_app.dart
@@ -1,13 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
-import 'package:flutter_easyloading/flutter_easyloading.dart';
 import 'package:flutter_weather_forecast_brazil/src/views/weather_forecast_brazil_home.dart';
 
 class WeatherForecastBrazilApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      builder: EasyLoading.init(),
       home: Scaffold(
         appBar: AppBar(
           title: Text('flutter_weather_forecast_brazil'),
